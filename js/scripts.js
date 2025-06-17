006e5e31507b2c03b8afe724a7efd2cb44e2f832
VladislavHTML <vladoz360@gmail.com>
2025-06-17 21:30:32 +0300

    new

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".burger");
+    const navigation = document.querySelector(".navigation");
+    const body = document.body;
+
+    function toggleMenu() {
+        const isActive = burger.classList.toggle("active");
+        navigation.classList.toggle("active");
+
+        if (isActive) {
+            lockScroll();
+        } else {
+            unlockScroll();
+        }
+    }
+
+    function lockScroll() {
+        body.classList.add("scroll-lock");
+    }
+
+    function unlockScroll() {
+        body.classList.remove("scroll-lock");
+    }
+
+    if (burger) {
+        burger.addEventListener("click", toggleMenu);
+    }
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     const track = document.querySelector('.slider__carts');
     const cards = document.querySelectorAll('.slider__cart');
